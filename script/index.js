7880ae12e4677f475b05d3e8305838585e70caae
Mamadou <opmadou@gmail.com>
2020-06-15 19:32:51 +0300

    Integration de la maquette, mise en place du responsive design

---
+++ script/index.js
@@ -0,0 +1,49 @@
+$(function () {
+  const NavBar = $(".navbar");
+  window.addEventListener("scroll", function (e) {
+    const scroll = this.scrollY;
+    if (scroll === 0) {
+      $(NavBar).removeClass("other");
+      $(NavBar).addClass("top");
+    } else {
+      $(NavBar).removeClass("top");
+      $(NavBar).addClass("other");
+    }
+  });
+
+  $(".previous").click(function () {
+    console.log("appelle");
+    const currentId = $("#active").data("index");
+    let index = 0;
+    if (currentId === 0) {
+      index = 4;
+    } else {
+      index = currentId - 1;
+    }
+    $("#active").attr("id", "");
+    $(`img[data-index=${currentId}]`).removeClass("active");
+    $(`.item[data-index=${index}]`).attr("id", "active");
+    $(".carousel-element").css(
+      "transform",
+      `translate3d(${-index * 100}%, 0px, 0px)`
+    );
+    $(`img[data-index=${index}]`).addClass("active");
+  });
+  $(".next").click(function () {
+    const currentId = $("#active").data("index");
+    let index = 0;
+    if (currentId === 4) {
+      index = 0;
+    } else {
+      index = currentId + 1;
+    }
+    $("#active").attr("id", "");
+    $(`img[data-index=${currentId}]`).removeClass("active");
+    $(`.item[data-index=${index}]`).attr("id", "active");
+    $(".carousel-element").css(
+      "transform",
+      `translate3d(${-index * 100}%, 0px, 0px)`
+    );
+    $(`img[data-index=${index}]`).addClass("active");
+  });
+});
